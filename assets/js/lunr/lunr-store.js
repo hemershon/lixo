da92aa6b12efa162b33bc245d0e8a5070bdbe859
Hemershon Silva <hemershon@gmail.com>
2022-12-24 18:03:01 -0300

    methods for this ruby

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,5 +1,5 @@
 var store = [{
-        "title": "Métodos interessantes para tratar seus array no Rails",
+        "title": "Métodos para tratar seus Array, Hashes, Ranges",
         "excerpt":"Recentemente me deparei em uma situação interessante, qual é o momento em que devemos usar map, select e o each, normalmente eu uso o each com frequência aí veio a dúvida em qual momento devemos usar os outros métodos e qual é a diferença entre eles? Procurando na documentação você...","categories": ["Rails"],
         "tags": ["rails","ruby","linux","metodos"],
         "url": "/rails/metodos-para-tratar-array/",
