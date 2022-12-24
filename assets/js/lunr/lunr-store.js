03c22527cc1fcae2c09f67db41f05f8345886152
Hemershon Silva <hemershon@gmail.com>
2022-12-24 14:34:46 -0300

    first commit for this blog

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Métodos interessantes para tratar seus array no Rails",
+        "excerpt":"Recentemente me deparei em uma situação interessante, qual é o momento em que devemos usar map, select e o each, normalmente eu uso o each com frequência aí veio a dúvida em qual momento devemos usar os outros métodos e qual é a diferença entre eles? Procurando na documentação você...","categories": ["Rails"],
+        "tags": ["rails","ruby","linux","metodos"],
+        "url": "/rails/metodos-para-tratar-array/",
+        "teaser": null
+      }]
